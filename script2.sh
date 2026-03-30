00a4560cbbca0b74d934832881acdf843dece34e
B Nehasri <burkanehasri@gmail.com>
2026-03-30 22:49:38 +0530

    Create script2.sh

---
+++ script2.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+echo "System Information"
+echo "-------------------"
+
+echo "User:"
+whoami
+
+echo "Current Directory:"
+pwd
+
+echo "System Uptime:"
+uptime
+
+echo "Disk Usage:"
+df -h
